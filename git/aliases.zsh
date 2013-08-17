5204d6ed60cab6a74419b1a98fd9d96ce414e64e
somethingnew2-0 <icuponq2@hotmail.com>
2013-08-17 10:27:18 -0700

    Update git alias.zsh with osaltzma changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,11 @@ then
   alias git=$hub_path
 fi
 
+#osaltzma git aliases
+gitg='git log --graph --decorate'
+gitgo='git log --graph --decorate --oneline'
+gits='git status'
+
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
